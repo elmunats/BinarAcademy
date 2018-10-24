f621bb1aeb15d63068e49d4bb2bc1e0c710e80ff
elmunats <elmunats@outlook.com>
2018-10-24 20:31:30 +0700

    Fitur Hitung

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,20 +2,9 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        int x = 8;
-        double y = 5;
-        double z;
 
-
-        System.out.println("Hello World\n");
-        System.out.println("X + Y = "+ (z=x+y) );
-        System.out.println("X - Y = "+ (x-y) );
-        System.out.println("X * Y = "+ (x*y));
-        System.out.println("X / Y = "+ (x/y));
-
-        y++;
-        System.out.println(z);
         input();
+        hitung();
 
 
     }
@@ -31,5 +20,21 @@ public class HelloWorld {
         System.out.print("Umur : ");
         Integer umur = input.nextInt();
         System.out.println("nama saya "+name+", tinggi badan saya "+tBadan+", umur saya "+umur+".");
+    }
+
+    public static void hitung(){
+        int x = 8;
+        double y = 5;
+        double z;
+
+
+        System.out.println("Hello World\n");
+        System.out.println("X + Y = "+ (z=x+y) );
+        System.out.println("X - Y = "+ (x-y) );
+        System.out.println("X * Y = "+ (x*y));
+        System.out.println("X / Y = "+ (x/y));
+
+        y++;
+        System.out.println(z);
     }
 }
